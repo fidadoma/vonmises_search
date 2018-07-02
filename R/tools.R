4c98ef9c1b0f289d20e3a0b488dd84806a42be06
fidadoma <filip.dechterenko@gmail.com>
2018-07-02 15:02:36 +0200

    create functions for figures

--- R/tools.R
+++ R/tools.R
@@ -67,3 +67,79 @@ compute.des <- function(dfx) {
   return(diff(means$Mean))
   
 }
+
+plot_fig2 <- function(restk_var, ggtit) {
+  df_exp1a %>%
+    filter(restk == restk_var) %>%
+    ggplot(aes(x = type, y = accuracy)) + 
+    stat_summary(fun.data = "mean_cl_boot") + 
+    stat_summary(fun.y = mean, geom = "line", aes(group = 1)) + 
+    geom_hline(yintercept=0.125) + 
+    theme(aspect.ratio = 1)  + xlab(expression("Target's"~kappa)) + 
+    ylab("Accuracy") + 
+    ylim(0,1)+
+    scale_x_continuous(expression("Target's"~kappa~" (log scale)"),breaks = c(1,8,16,32,64,128),labels = c("1","8","16","32","64","128"), trans = "log2") + 
+    geom_vline(xintercept = restk_var, linetype = 2) +
+    ggtitle(ggtit)
+}
+
+plot_fig3 <- function(restk_var, show_legend = F) {
+  
+  p <- df2_exp1b %>%
+    filter(restk == restk_var) %>%
+    ggplot(aes(x = type, y = accuracy, group = version)) + 
+    #facet_grid(~ restk) + 
+    stat_summary(fun.data = "mean_cl_boot") + 
+    stat_summary(fun.y = mean, geom = "line", aes(group = version, linetype = version)) + 
+    geom_hline(yintercept=0.125) +
+    theme(aspect.ratio = 1) + 
+    ylab("Accuracy") + 
+    ylim(0,1)+
+    scale_x_continuous(expression("Variable"~kappa~" (log scale)"),breaks = c(2,4,8,16,64),labels = c("2","4","8","16","64"), trans = "log2") + 
+    scale_linetype_manual(values = c(2,1)) +
+    geom_vline(xintercept = restk_var, linetype = 2) +
+    ggtitle(bquote("Fixed "~kappa~"= "~.(restk_var)))
+  
+  if (show_legend) {
+    p 
+  } else {
+    p + theme(legend.position="none")
+  }
+  
+}
+
+plot_fig4 <- function(restspd, ggtit) {
+  df_exp2a %>%
+    filter(restk == restspd) %>%
+    ggplot(aes(x = type, y = accuracy)) + 
+    stat_summary(fun.data = "mean_cl_boot") + 
+    stat_summary(fun.y = mean, geom = "line", aes(group = 1)) + 
+    geom_hline(yintercept=0.125) + 
+    theme(aspect.ratio = 1) + 
+    ylab("Accuracy") + 
+    ylim(0,1)+
+    geom_vline(xintercept = restspd, linetype = 2) +
+    scale_x_continuous("Target's speed (log scale)",breaks = c(0.5,1,2,3,4,6,8),labels = c("0.5","1","2","3","4","6","8"), trans = "log2") + 
+    ggtitle("Slow-moving-distractors")
+}
+
+plot_fig5 <- function(restspd, show_legend = F) {
+  p <- df2_exp2b %>%
+    filter(restk == restspd) %>%
+    ggplot(aes(x = type, y = accuracy, group = version)) + 
+    stat_summary(fun.data = "mean_cl_boot") + 
+    stat_summary(fun.y = mean, geom = "line", aes(group = version, linetype = version)) + 
+    geom_hline(yintercept=0.125) +
+    theme(aspect.ratio = 1) + 
+    ylab("Accuracy") + 
+    ylim(0,1)+
+    scale_x_continuous("Variable speed (log scale)",breaks = c(.5,1,2,4,8),labels = c(".5","1","2","4","8"),
+                       trans = "log2") + 
+    geom_vline(xintercept = restspd, linetype = 2) +
+    ggtitle(bquote("Fixed speed = "~.(restspd)*degree*"/s"))
+  if (show_legend) {
+    p 
+  } else {
+    p + theme(legend.position="none")
+  }   
+}
